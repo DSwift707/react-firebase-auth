b787e3d6860841f180d3f2bddebd5530cf08ebff
DSwift707 <devonswift@sbcglobal.net>
2021-05-10 12:14:31 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import app from "./base";
 import { AuthContext } from "./Auth";
+import PinTree from "./components/PinTree";
+import SimpleMap from "./components/GoogleMap";
 
 const Home = () => {
   const {currentUser} = useContext(AuthContext);
@@ -8,6 +10,7 @@ const Home = () => {
     <>
       <h1>Home</h1>
       <button onClick={() => app.auth().signOut()}>Sign out of {currentUser.email}</button>
+      <SimpleMap />
     </>
   );
 };
