247ffb88dd05563d28e8f2ca3b88866c697e8112
DSwift707 <devonswift@sbcglobal.net>
2021-05-10 16:45:53 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,16 @@ import app from "./base";
 import { AuthContext } from "./Auth";
 import PinTree from "./components/PinTree";
 import SimpleMap from "./components/GoogleMap";
+import { Button } from "@material-ui/core";
 
 const Home = () => {
   const {currentUser} = useContext(AuthContext);
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={() => app.auth().signOut()}>Sign out of {currentUser.email}</button>
+      <div className="main-header">
+        <h1 className="main-title">Forestree.io</h1>
+        <Button color="default" variant="contained" className="logout-button" onClick={() => app.auth().signOut()}>Log Out</Button>
+      </div>
       <SimpleMap />
     </>
   );
